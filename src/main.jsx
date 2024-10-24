5f113a11dbdb7e9aea57c7fa58ce777a0a47905d
Olayuwa Timilehin Ebenezer <timileyinolayuwa@gmail.com>
2024-10-24 16:38:45 -0700

    Added facebook pixel

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import FacebookPixel from "./sections/FacebookPixel.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <FacebookPixel />
     <App />
   </StrictMode>,
 );
